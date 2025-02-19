df77f5df89053df0ebea9a777b61215a5ef32483
Seunghwan Moon <sh97818@gmail.com>
2025-02-19 09:06:31 +0900

    Update activate.py

--- ML/activate.py
+++ ML/activate.py
@@ -1,7 +1,9 @@
 import numpy as np
 
+# Activation Function
 def relu(z):
     return np.maximum(0, z)
 
+# Derivative of Activation Function
 def relu_derivative(z):
     return (z > 0).astype(float)
